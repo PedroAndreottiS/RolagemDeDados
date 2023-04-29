77b3c71542b45e093e3266473b8944b264ebd0a8
ETEC Adolpho Berezin <e107ti@etec.sp.gov.br>
2023-04-28 22:22:17 -0300

    Exercicio finalizado

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Security.Cryptography;
+int quantidadeDeFaces;
+Console.WriteLine("---Rolagem de Dados!---");
+Console.WriteLine("Insira a quantidade de faces do dado");
+quantidadeDeFaces = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("inciando O PROCESOO...");
+
+
+int Rolagem = RandomNumberGenerator.GetInt32(1, quantidadeDeFaces);
+
+Console.WriteLine($"{Rolagem}");
+
